868c827cf9e6ac111b55f369534db05db77584e6
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2024-12-06 17:05:59 +0530

    feat: Added multi language feature

--- frontend/src/pages/CodeEditor.tsx
+++ frontend/src/pages/CodeEditor.tsx
@@ -1,4 +1,4 @@
-import React,{useRef,useEffect} from 'react'
+import React,{useRef,useEffect, useState} from 'react'
 import * as monaco from 'monaco-editor';
 import 'monaco-editor/esm/vs/editor/editor.worker';
 import 'monaco-editor/esm/vs/language/typescript/ts.worker';
@@ -7,6 +7,7 @@ import 'monaco-editor/esm/vs/language/html/html.worker';
 import 'monaco-editor/esm/vs/language/css/css.worker';
 
 function CodeEditor() {
+const [language, setLanguage] = useState('javascript');
 const editorRef = useRef<HTMLDivElement | null>(null)
 const monacoInstance = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
@@ -24,8 +25,31 @@ useEffect(() => {
       };
 },[]);
 
+useEffect(() => {
+    if (monacoInstance.current) {
+      monaco.editor.setModelLanguage(monacoInstance.current.getModel()!, language);
+    }
+  }, [language]);
+
   return (
+    <div>
+    <div className='bg-black p-2'>
+        select language:
+        <select
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+        >
+          <option value="javascript">JavaScript</option>
+          <option value="typescript">TypeScript</option>
+          <option value="json">JSON</option>
+          <option value="html">HTML</option>
+          <option value="css">CSS</option>
+          <option value="python">Python</option>
+        </select>
+    </div>
     <div ref={editorRef} style={{height: '100vh',width: '100%'}}/>
+
+    </div>
   )
 }
 
